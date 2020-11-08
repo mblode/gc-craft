b9873d18fbd320814a35a6e7d2a447f79746883b
Matthew Blode <m@blode.co>
2020-11-08 15:16:59 +1100

    Initial commit

---
+++ config/general.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\helpers\App;
+
+return [
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
+
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
+
+        // Control panel trigger word
+        'cpTrigger' => 'admin',
+
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => App::env('SECURITY_KEY'),
+
+        'headlessMode' => true,
+
+        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
+        'aliases' => [
+            '@web' => App::env('SITE_URL'),
+        ],
+
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+        'devMode' => true,
+
+        // Prevent crawlers from indexing pages and following links
+        'disallowRobots' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Set this to `false` to prevent administrative changes from being made on Staging
+        'allowAdminChanges' => true,
+
+        // Don’t allow updates on Staging
+        'allowUpdates' => false,
+
+        // Prevent crawlers from indexing pages and following links
+        'disallowRobots' => true,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Set this to `false` to prevent administrative changes from being made on Production
+        'allowAdminChanges' => false,
+
+        // Don’t allow updates on Production
+        'allowUpdates' => false,
+    ],
+];
